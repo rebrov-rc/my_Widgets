ad3d8b681b1a37e3cce5041145e985fa73b531fd
Roman <rebrovdev@gmail.com>
2020-07-04 18:37:46 +0300

    18.37

--- JS/rate.js
+++ JS/rate.js
@@ -1,49 +1,48 @@
 "use strict"
-let lStorage = localStorage.getItem('rate')
+function onLoad(){
+    let lStorage = localStorage.getItem('rate'),
+    block = document.querySelector('.stars-wrap');
+    block.addEventListener('mouseout' , post)
+    // block.addEventListener('mouseover' ,  () => {  })
+    for ( let i = 0; i < 5; i++ ){
+        block.innerHTML +=`<div class='st-wrap flex'> <i class="fa fa-star stars" aria-hidden="true"></i></div>`
+    }
 
-let block = document.querySelector('.stars-wrap');
-
-for ( let i = 0; i < 5; i++ ){
-    block.innerHTML +=`<div class='st-wrap flex'> <i class="fa fa-star stars" aria-hidden="true"></i></div>`
-}
-
-let stars = document.querySelectorAll('.stars'),
-stWrap = document.querySelectorAll('.st-wrap');
-if( lStorage === null ){lStorage = 0}
-
-function storage(){
-    for( let iII = 0;  iII < lStorage ; iII++ ){
-        stars[iII].classList.add('green')
+    let stars = document.querySelectorAll('.stars'),
+    stWrap = document.querySelectorAll('.st-wrap');
+    if( lStorage === null ){lStorage = 0}
+    ////////////
+    storage()
+    ppp()
+    //////////////////////////////////////////////////////
+    function storage(){
+        for( let iII = 0;  iII < lStorage ; iII++ ){
+            stars[iII].classList.add('green')
+        }
+    }
+    function post() { 
+        stars.forEach( item => {
+            item.classList.remove('green')
+        })
+        storage()
+    }
+    function ppp(){
+        stWrap.forEach((item, i) => {
+            item.addEventListener('click' ,  () => { 
+                lStorage = i + 1
+                localStorage.setItem('rate', lStorage)
+                post()
+            })
+            item.addEventListener('mouseover' , function choise() {
+        
+                for( let iII = 0;  iII < i + 1 ; iII++ ){
+                    stars[iII].classList.add('green')
+                }
+            })
+            // item.addEventListener('mouseout' ,  () => { 
+            //  for( let iII = 0;  iII < i + 1 ; iII++ ){stars[iII].classList.remove('green')}})
+        })
+        
     }
 }
-storage()
-
-block.addEventListener('mouseout' ,  () => { 
-    stars.forEach( item => {
-        item.classList.remove('green')
-    })
-    storage()
- })
-block.addEventListener('mouseover' ,  () => {  })
-ppp()
-function ppp(){
-    stWrap.forEach((item, i) => {
-        item.addEventListener('click' ,  () => { 
-              
-            lStorage = i + 1
-            localStorage.setItem('rate', lStorage)
-        })
-        item.addEventListener('mouseover' , function choise() {
-    
-            for( let iII = 0;  iII < i + 1 ; iII++ ){
-                stars[iII].classList.add('green')
-            }
-        })
-        item.addEventListener('mouseout' ,  () => { 
-            for( let iII = 0;  iII < i + 1 ; iII++ ){
-                stars[iII].classList.remove('green')
-            }
-        })
-    })
-    
-}
+onLoad()
