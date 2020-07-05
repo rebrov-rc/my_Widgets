e5b3993610b35a712c35c037a856ca7b87d9f146
Roman <rebrovdev@gmail.com>
2020-07-05 14:01:12 +0300

    14.01

--- JS/clock copy.js
+++ JS/clock copy.js
@@ -3,7 +3,14 @@ class TimeClass{
     constructor(s, m, h){
         this.secLine = document.querySelector(s),
         this.minLine = document.querySelector(m),
-        this.hourLine = document.querySelector(h)
+        this.hourLine = document.querySelector(h),
+        this.getTime = new Date,
+        this.getHour = this.getTime.getHours(),
+        this.getMinutes = this.getTime.getMinutes(),
+        this.getSec = this.getTime.getSeconds(),
+        this.secCounter = this.getSec, 
+        this.minCounter = this.getMinutes * 6,   
+        this.hourCounter = this.getHour * 30;
     };
     timeData() {
         this.getTime = new Date,  
@@ -16,12 +23,9 @@ class TimeClass{
     };
     start() {
         let interval = setInterval(() => {this.stconds()}, 1000)
-        this.timeData()
-        this.check()
     };
     check(){
-        setInterval(() => { this.timeData(); console.log(456);
-         }, 3600)
+        setInterval(() => { this.timeData() }, 3600000)
     };
     stconds() {
         let dependCount = this.minCounter + this.secCounter / 10,
@@ -40,6 +44,8 @@ class TimeClass{
         if ( this.hourCounter === 360 ){ 
             this.hourCounter = 0
         }
+        // console.log(this);
+        this.check()
     };
 }
 const oClock = new TimeClass('.seconds', '.minutes', '.hours')
