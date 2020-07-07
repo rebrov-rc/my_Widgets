02f432e600d94839cd8443e9b1274f72398b7bf0
Roman <rebrovdev@gmail.com>
2020-07-07 22:17:52 +0300

    07.07 22.17

---
+++ JS/slider-I.js
@@ -0,0 +1,28 @@
+const products = {
+    array: [
+        {
+            id: 01,
+            img: [''],
+            name: '',
+            description: {
+                title: ''
+            },
+            prise: {
+                new: '',
+                old: ''
+            }
+        },
+        {
+            id: 02,
+            img: [''],
+            name: '',
+            description: {
+                title: ''
+            },
+            prise: {
+                new: '',
+                old: ''
+            }
+        }
+    ]
+}
