2b6aa677cc62184a5f2c6f70d5252d657dacfeeb
Roman <rebrovdev@gmail.com>
2020-07-09 14:26:07 +0300

    114.26

---
+++ JS/slider-II.js
@@ -0,0 +1,34 @@
+const productsII = {
+    array: [
+        {
+            id: 01,
+            img: [''],
+            name: '',
+            description: {
+                title: ''
+            },
+            prise: {
+                new: '',
+                old: ''
+            }
+        },
+        {
+            id: 02,
+            img: [''],
+            name: '',
+            description: {
+                title: ''
+            },
+            prise: {
+                new: '',
+                old: ''
+            }
+        }
+    ]
+}
+
+class SliderII {
+    constructor(){
+        
+    }
+}
