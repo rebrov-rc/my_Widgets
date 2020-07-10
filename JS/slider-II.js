d02fd9e76a0ebeaa3fea7a231e1a82b651630d26
Roman <rebrovdev@gmail.com>
2020-07-10 18:05:04 +0300

    18.05

--- JS/slider-II.js
+++ JS/slider-II.js
@@ -2,7 +2,7 @@ const productsII = {
     array: [
         {
             id: 01,
-            img: [''],
+            img: ['https://lh3.googleusercontent.com/proxy/yQnobQ8jqpc0z-coy9x5xb2lipMK0MRKCDUyS3WPZU1DlrxnrQHYeMqiZV-0AgtIQ-neOYhyKlUTa8UZcHqHTqwdvetuGjWvQ1dkw0EnW_fy8YXzjBfqW_k'],
             name: '',
             description: {
                 title: ''
@@ -14,7 +14,31 @@ const productsII = {
         },
         {
             id: 02,
-            img: [''],
+            img: ['https://lh3.googleusercontent.com/proxy/l549Y9n8vYQFwGqKPl4DfyMZiW_el7UK1pGeO2ntC60GDnT7yw5_fqT_gxCbzxiZ9CE3glLs3gn9VOf5s9rQqY9_ovV74EtWmSCQhrTiJwjAbXi7Rb6HeN4-S82JHlTIC8JpsxCLkN477JHTjdFw3N8i7fbAq_42dSg3wMU'],
+            name: '',
+            description: {
+                title: ''
+            },
+            prise: {
+                new: '',
+                old: ''
+            }
+        },
+        {
+            id: 03,
+            img: ['https://lh3.googleusercontent.com/proxy/8wH7zKzAUubdmXCPehKXdoXxKIpwyV6EYEpUIsqimN9Bpkz036NbLUj0TN2IeJ9HMVp9p7I90GgID82F4wB2GoXXt_ywUPy90rDQTKbitVCZ9RLXrDMCsLfZos1f_4gtij69iYU'],
+            name: '',
+            description: {
+                title: ''
+            },
+            prise: {
+                new: '',
+                old: ''
+            }
+        },
+        {
+            id: 04,
+            img: ['https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRS6__jkfNWjDiw9ggnKcdieqO9LUBmKWr3Eg&usqp=CAU'],
             name: '',
             description: {
                 title: ''
@@ -28,7 +52,44 @@ const productsII = {
 }
 
 class SliderII {
-    constructor(){
-        
+    constructor(prod, wrap, elements){
+      this.prod = prod
+      this.wrap = document.querySelector('.' + wrap)
+      this.el = document.querySelector('.' + elements)
+    };
+    clean(disp){
+        for(let i = 0; i < this.prod.length; i++ ){
+            this.retEl[i].classList.remove('sl-ii--active')
+            this.retEl[i].style.display = disp
+            this.wrap.children[0].src = this.prod[0].img
+        }
+    }
+    start(){
+            this.wrap.children[0].src = this.prod[0].img
+            for(let i = 0; i < this.prod.length; i++){
+                this.el.innerHTML += `<div class='items'>
+                    <div class='return-elements-wrap'>
+                        <div class='return-elements'></div>
+                    </div>
+                </div>`
+            }
+
+            this.events()
+    }
+    events(){
+        this.items = document.querySelectorAll('.items')
+        this.retEl = document.querySelectorAll('.return-elements')
+        this.wrap.addEventListener('mouseout', () => this.clean('none'))
+        this.items.forEach((item, i) => {
+            item.addEventListener('mouseover', () => { this.change(i) })
+        });
+    }
+    change(i){
+        this.clean('block')
+        this.retEl[i].classList.add('sl-ii--active')
+        this.wrap.children[0].src = this.prod[i].img
     }
 }
+
+const slider = new SliderII(productsII.array, 'slider--II', 'returnElements')
+slider.start()
