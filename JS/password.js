fc06c91a5caac75e1b4a57c75bb9e1c89eabbf96
Roman <rebrovdev@gmail.com>
2020-07-19 19:02:03 +0300

    19,01

--- JS/password.js
+++ JS/password.js
@@ -1,13 +1,28 @@
-class Pass {
+class User{
+    constructor(name){
+        this.name = name
+    }
+    get _password(){}
+}
+class Pass extends User{
     constructor(){
+        super()
         this.inp = document.querySelector('.inp-pass')
         this.eye = document.querySelector('.icon-pass')
+        this.info = document.querySelector('.pass-info')
+        this.emos = document.querySelector('.emotions-wrap').children
+        this.hidden = true
         this.len = []
+        this.prop = []
+        this.complexity = ['.',',',';',':','/','*','=','&','%','?','$']
     }
     init(){
-        console.log(this.len.length, this.inp.value.length)
         this.eye.addEventListener('click', () => {this.events()})
         this.inp.addEventListener('input', () => {this.hadler()})
+        this.prop[0] = 'initial'
+        this.prop[1]  = 'none'
+        this.icons()
+
     };
     hadler(){
         if(this.len.length < this.inp.value.length ){
@@ -15,27 +30,80 @@ class Pass {
         }else{
             this.len.pop( this.inp.value[this.inp.value.length-1]) 
         }
-        // console.log(this.inp.value[this.inp.value.length-1]);
-        this.inp.value = ''
-        for( let i = 0; i < this.len.length; i++ ){
-            this.inp.value += '*'
+        if(this.hidden === true){
+            this.inHidden()
         }
-
+        this.emosHandler(this.len)
     };
     events(){
         this.eye.classList.toggle('active')
         let checking = this.eye.classList.value.indexOf('active')
-        console.log(checking);
+        // console.log(checking);
         if (checking != -1){
+            [this.prop[0],this.prop[1]]  = ['none' , 'initial']
+            this.icons()
             this.switch()
+            this.hidden = false
+        }else{ 
+            [this.prop[0],this.prop[1]]  = ['initial' , 'none']
+            this.icons()
+            this.inHidden() 
+            this.hidden = true
         }
     };
     switch(){
-        // let 
         this.inp.value = ''
         for( let i = 0; i < this.len.length; i++ ){
             this.inp.value += this.len[i]
         }
+    };
+    inHidden(){
+        this.inp.value = ''
+        for( let i = 0; i < this.len.length; i++ ){
+            this.inp.value += '*'
+        }
+    };
+    icons(){
+        this.eye.children[0].style.display = this.prop[0]
+        this.eye.children[1].style.display = this.prop[1]
+    }
+    emosHandler(value){
+        let complexityReturn = false;
+        value.forEach(item => {
+            if(this.complexity.indexOf(item) != -1){
+                complexityReturn = true
+            }
+        });
+        if(value.length === 0 ){
+            this.emos[0].style.display = 'none'
+            this.emos[1].style.display = 'none'
+            this.emos[2].style.display = 'none'
+            this.info.innerText = 'Enter password'
+        }else if (value.length < 6){
+            this.info.innerText = 'Пароль слишком лёгкий'
+            this.emos[0].style.display = 'block'
+            this.emos[1].style.display = 'none'
+            this.emos[2].style.display = 'none'
+        }else if ( complexityReturn === true && value.length >= 8 || complexityReturn === false && value.length > 10 ){
+            this.emos[0].style.display = 'none'
+            this.emos[1].style.display = 'none'
+            this.emos[2].style.display = 'block'
+            this.info.innerText = 'Отлично!'
+
+        }
+        else if ( value.length > 5 && value.length < 8 ){
+            this.emos[1].style.display = 'block'
+            this.emos[0].style.display = 'none'
+            this.emos[2].style.display = 'none'
+            this.info.innerText = 'Добавьте сомволов / ? * & = % $'
+
+        }else{
+            this.emos[1].style.display = 'block'
+            this.emos[0].style.display = 'none'
+            this.emos[2].style.display = 'none'
+            this.info.innerText = 'Добавьте сомволов / ? * & = % $'
+        }
+        
     }
 }
 // --------------
